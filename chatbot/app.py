65e88856c3447ac0f17700c54fab3e363a463e48
mroreoman <830aryans@gmail.com>
2024-11-02 20:12:37 -0400

    gitignore

--- chatbot/app.py
+++ chatbot/app.py
@@ -3,16 +3,17 @@ import requests
 import os
 
 dotenv.load_dotenv()
-ACCOUNT_ID = os.environ.get("ACCOUNT_ID")
+ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
 AUTH_TOKEN = os.environ.get("CLOUDFLARE_AUTH_TOKEN")
 
 SYSTEM_PROMPT = "You are Octavius, a steampunk-themed shopkeeper. Help your customers to find parts in the shop and answer their questions about how to fix their machines."
+MODEL = "@hf/thebloke/llama-2-13b-chat-awq"
 
 myhistory = []
 
 def prompt(message, history):
     response = requests.post(
-    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-3.1-8b-instruct",
+    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/{MODEL}",
         headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
         json={
             "messages": [
