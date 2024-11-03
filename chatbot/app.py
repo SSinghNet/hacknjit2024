017f58e4a4c6008f0851ee51c292822410424a49
mroreoman <830aryans@gmail.com>
2024-11-03 01:59:53 -0500

    skeleton for chatbot api

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,40 +1,16 @@
-import dotenv
-import requests
-import os
+from flask import Flask, jsonify, request
+import chatbot
+  
+app = Flask(__name__)
+  
+# on the terminal type: curl http://127.0.0.1:5000/ 
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/', methods = ['GET', 'POST']) 
+def prompt(): 
+    if(request.method == 'POST'): 
+        data = request.get_json()
+        return chatbot.prompt(data['prompt'], data['message_history'])
 
-dotenv.load_dotenv()
-ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
-AUTH_TOKEN = os.environ.get("CLOUDFLARE_AUTH_TOKEN")
-
-SYSTEM_PROMPT = "You are Octavius, a steampunk-themed shopkeeper. Help your customers to find parts in the shop and answer their questions about how to fix their machines."
-MODEL = "@hf/thebloke/llama-2-13b-chat-awq"
-
-myhistory = []
-
-def prompt(message, history):
-    response = requests.post(
-    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/{MODEL}",
-        headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
-        json={
-            "messages": [
-                {"role": "system", "content": SYSTEM_PROMPT},
-                *[{"role": "user", "content": m} for m in history],
-                {"role": "user", "content": message}
-            ]
-        }
-    )
-    return response.json()['result']['response']
-
-while True:
-    # print("HISTORY")
-    # for i, m in enumerate(myhistory):
-    #     print(f"\t{i+1}: {m}")
-
-    message = input("your message: ")
-    myhistory.append(message)
-    print()
-
-    response = prompt(message, myhistory)
-    print(response)
-    myhistory.append(response)
-    print()
+if __name__ == '__main__': 
+    app.run(debug = True) 
