835a1ef51da4d3f427a095d2a4ab69de981b34fc
mroreoman <830aryans@gmail.com>
2024-11-03 09:30:22 -0500

    temp working chatbot

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request
-import chatbot
+from chatbot import Chatbot
   
 app = Flask(__name__)
+cb = Chatbot()
   
-# on the terminal type: curl http://127.0.0.1:5000/ 
-# returns hello world when we use GET. 
-# returns the data that we send when we use POST. 
 @app.route('/', methods = ['GET', 'POST']) 
 def prompt(): 
     if(request.method == 'POST'): 
         data = request.get_json()
-        return chatbot.prompt(data['prompt'], data['message_history'])
+        return cb.prompt(data['prompt'], data['message_history'])
 
 if __name__ == '__main__': 
     app.run(debug = True)
--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,45 +1,75 @@
 import dotenv
-import requests
 import os
+import requests
+import json
 
-dotenv.load_dotenv()
-ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
-AUTH_TOKEN = os.environ.get("CLOUDFLARE_AUTH_TOKEN")
+class Chatbot:
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
+        self.AUTH_TOKEN = os.environ.get("CLOUDFLARE_AUTH_TOKEN")
 
-system_prompt = "You are Octavius, a steampunk-themed shopkeeper. Help your customers to find parts in the shop and answer their questions about how to fix their machines."
-model = "@hf/thebloke/llama-2-13b-chat-awq"
-max_tokens = 256
+        self.system_prompt = "You are Octavius, a steampunk-themed shopkeeper. Help your customers to find parts in the shop and answer their questions about how to fix their machines."
+        self.model = "@hf/thebloke/llama-2-13b-chat-awq"
+        # self.model = "@hf/nousresearch/hermes-2-pro-mistral-7b"
+        self.max_tokens = 256
 
-def prompt(message, history):
-    response = requests.post(
-    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/{model}",
-        headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
-        json={
-            "messages": [
-                {"role": "system", "content": system_prompt},
-                *history,
-                {"role": "user", "content": message}
-            ],
-            "max_tokens" : max_tokens
-        }
-    )
-    response.raise_for_status()
-    return response.json()['result']['response']
+    def getAllItems(self):
+        response ="""[
+{
+    "id": "item1",
+    "name": "Iron Gear",
+    "description": "A simple iron gear"
+},
+{
+    "id": "item2",
+    "name": "Brass Pipe",
+    "description": "A fancy brass pipe"
+}
+]"""
+        items = json.loads(response)
+        # for item in items:
+        #     item.pop("id")
+        return items
 
-def runInTerminal():
-    myhistory = []
+    def prompt(self, message, history):
+        response = requests.post(
+        f"https://api.cloudflare.com/client/v4/accounts/{self.ACCOUNT_ID}/ai/run/{self.model}",
+            headers={"Authorization": f"Bearer {self.AUTH_TOKEN}"},
+            json={
+                "messages": [
+                    {"role": "system", "content": self.system_prompt},
+                    *history,
+                    {"role": "user", "content": message}
+                ],
+                # "tools": [
+                #     {
+                #     "name": "getAllItems",
+                #     "description": "Provides a list of items with their ID, name, and description.",
+                #     "parameters": {}
+                #     }
+                # ],
+                "max_tokens" : self.max_tokens
+            }
+        )
+        response.raise_for_status()
+        return response.json()['result']['response']
 
-    while True:
-        message = input("your message: ")
-        response = prompt(message, myhistory)
-        print("\n" + response + "\n")
-        myhistory.append({"role": "user", "content": message})
-        myhistory.append({"role": "assistant", "content": response})
+    def runInTerminal(self):
+        myhistory = []
 
-        # print("HISTORY")
-        # for i, m in enumerate(myhistory):
-        #     print(f"\t{i+1}: {m}")
-        # print()
+        while True:
+            message = input("your message: ")
+            response = self.prompt(message, myhistory)
+            print("\n" + response + "\n")
+            myhistory.append({"role": "user", "content": message})
+            myhistory.append({"role": "assistant", "content": response})
+
+            # print("HISTORY")
+            # for i, m in enumerate(myhistory):
+            #     print(f"\t{i+1}: {m}")
+            # print()
 
 if __name__ == '__main__':
-    runInTerminal()
+    cb = Chatbot()
+    cb.runInTerminal()
